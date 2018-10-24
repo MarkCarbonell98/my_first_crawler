fe61d41b2a7f23da8afd5020bc8f7b3fbbcc6450
Carbonell <m.carbonell@trio-group.de>
2018-10-24 10:05:29 +0200

    crawler2.0 is finished ... for now

---
+++ crawler2.0.js
@@ -0,0 +1,44 @@
+const fetch = require("fetch");
+const urls = ['M7171_en', 'M7151_en', 'M7131_en'];
+let allFetchedData = [], allData = {};
+
+const getDataAsJSON = async url => {
+    const baseUrl = url;
+    const options = {
+        method: "GET", 
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    const theRequest = await fetch(baseUrl, options);
+    const theResponse = await theRequest.json();
+    return theResponse;
+}
+
+
+
+const getAllUrlsAsJSON = async (address, allLinks) => {
+    const baseUrl = address;
+    const allUrls = [...allLinks];
+
+    const allPromises = await Promise.all(allUrls.map(async url => {
+        const theRequestParsed = await getDataAsJSON(`${baseUrl}${url}`);
+        allFetchedData.push(theRequestParsed);
+        allData[url] = theRequestParsed;
+        return theRequestParsed;
+    }));
+    
+    return allPromises;
+}
+
+const logEverything = async (theBaseUrl, ArrayOfLinks) => {
+    const theUrl = theBaseUrl || '';
+    const theLinks = ArrayOfLinks || [];
+    const theFinalRequest = await getAllUrlsAsJSON(theUrl, theLinks);
+    console.log({theFinalRequest});
+    console.log("%c and the logged data is: ", "color: yellow; font-size: 20px; font-weight: bold;");
+    console.log({allFetchedData, allData});
+    return "The data was successfully retrieved!";
+}
+
+logEverything("https://touch-kubota.trio-hosting.de/api/", urls);
